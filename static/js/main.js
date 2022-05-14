9d9409fda225c8a71b53df08597c9b67deda1636
grobolsek <grobolsek.matic04@gmail.com>
2022-05-15 00:26:11 +0200

    added notes and css, js for notes

--- static/js/main.js
+++ static/js/main.js
@@ -1,9 +1,22 @@
 $(document).ready(function(){
-	console.log( "js is working" )
+    console.log("js in working")
+	// add list with button
 	$( "#addList" ).click(function() {
 		$( "#main" ).append( '<ol class="list"></ol>' );
 		$( ".list:last-child" ).append( "<li>Appended item</li>" );
 	});
 
+	// change div for br when creating new line
 	document.execCommand("defaultParagraphSeparator", false, "br");
+
+	// placeholder for note
+	jQuery(function($){
+        $("[contenteditable]").focusout(function(){
+            const element = $(this);
+            if (!element.text().trim().length) {
+                element.empty();
+            }
+        });
+    });
+
 });
